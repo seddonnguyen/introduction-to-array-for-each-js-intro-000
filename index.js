4a46ece9d88605193d48cd6aa0cb3c78741399a1
Tri Nguyen <tri.seddon.nguyen@gmail.com>
2019-04-16 23:15:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Add your doToElementsInArray() function here:
-function doToElementsInArray() {
+function doToElementsInArray(array, callback) {
 
 }
 
 // Add your changeCompletely() function here:
 function changeCompletely() {
-  
+
 }
