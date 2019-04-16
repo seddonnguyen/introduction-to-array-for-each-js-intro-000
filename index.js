03134277bdefb24fbf15f54888f8babbd32184da
Tri Nguyen <tri.seddon.nguyen@gmail.com>
2019-04-16 23:20:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function doToElementsInArray(array, callback) {
 }
 
 // Add your changeCompletely() function here:
-function changeCompletely() {
-
+function changeCompletely(element, index, array) {
+  array[index] = `${element}s!!!`;
 }
