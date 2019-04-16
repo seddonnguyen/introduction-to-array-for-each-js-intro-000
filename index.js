04939a20f32255ec81e3d771b0b792d4f6230481
Tri Nguyen <tri.seddon.nguyen@gmail.com>
2019-04-16 23:17:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your doToElementsInArray() function here:
 function doToElementsInArray(array, callback) {
-  array.foreach(callback);
+  array.forEach(callback);
 }
 
 // Add your changeCompletely() function here:
